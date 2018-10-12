a60ec69b462777566d985eae4d7fe8da9760f9cd
Rane M Kaseman <raykman@comcast.net>
2018-10-11 22:04:23 -0600

    2204

---
+++ scripts/userDB.js
@@ -0,0 +1,28 @@
+
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/userdb"
+);
+
+const userSeed = [
+    {
+        username: "username",
+        password: "password",
+        date: new Date(Date.now())
+    }
+];
+
+db.User
+    .remove({})
+    .then(() => db.User.collection.insertMany(userSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
--- server.js
+++ server.js
@@ -1,10 +1,53 @@
 
 const express = require("express");
 
+
+// +
+// const passport = require('passport');
+// const Strategy = require('passport-local').Strategy;
+// ^
+
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+// +
+// passport.use(new Strategy(
+//     function (username, password, cb) {
+//         db.users.findByUsername(username, function (err, user) {
+//             if (err) { return cb(err); }
+//             if (!user) { return cb(null, false); }
+//             if (user.password != password) { return cb(null, false); }
+//             return cb(null, user);
+//         });
+//     }));
+// passport.serializeUser(function (user, cb) {
+//     cb(null, user.id);
+// });
+
+// passport.deserializeUser(function (id, cb) {
+//     db.users.findById(id, function (err, user) {
+//         if (err) { return cb(err); }
+//         cb(null, user);
+//     });
+// });
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+// app.use(require('morgan')('combined'));
+// app.use(require('cookie-parser')());
+// app.use(require('body-parser').urlencoded({ extended: true }));
+// app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+// app.use(passport.initialize());
+// app.use(passport.session());
+// ^
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +61,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/user");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb");
 
 // Start the API server
 app.listen(PORT, function () {
