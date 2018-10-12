a1c7cfa4dc0e40aaeea676e22d5f86cea27df155
Rane M Kaseman <raykman@comcast.net>
2018-10-11 19:13:19 -0600

    1913

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -10,6 +11,7 @@ const App = () => (
     <div>
       <Nav />
       <Switch>
+        <Route exact path="/login" component={Login} />
         <Route exact path="/" component={Books} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/books/:id" component={Detail} />
